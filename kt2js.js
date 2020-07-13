67d47ecd71336e1002d50397d4a3d486f9abfe21
SungBin <sungbin.dev@gmail.com>
2020-07-13 12:56:09 +0900

    Update kt2js.js
    No Test

--- kt2js.js
+++ kt2js.js
@@ -6,7 +6,7 @@ const scriptName = "kt2js";
  * Copyright (C) SungBin
  * all rights reserved.
  */
- 
+
 String.prototype.convert = function(argu) {
     return eval(("\"" + this.match(/\$\{([^}]*)\}|\$([^\W]*)|\}?([^$]*)/gim).map(e => {
         if (typeof e !== "string") return "";
@@ -20,6 +20,75 @@ String.prototype.convert = function(argu) {
     }).join("") + "\"").split("\n").join("☆♡♡☆")).split("☆♡♡☆").join("\n");
 };
 
-function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-    replier.reply(msg.convert());
+Array.prototype.size = function() {
+    return this.length;
+};
+
+Array.prototype.isEmpty = function() {
+    return this.size() > 0;
+};
+
+Array.prototype.isNotEmpty = function() {
+    return !this.isEmpty();
+};
+
+Array.prototype.contains = function(text) {
+    return this.includes(text);
+};
+
+Array.prototype.clear = function() {
+    return [];
 }
+
+Array.prototype.remove = function(element, removeAll) {
+    if (!this.contains(element)) return this;
+    if (!removeAll) {
+        this.splice(index, 1);
+        return this;
+    } else {
+        this.remove(element);
+        if (this.contains(element)) this.remove(element, true);
+        else return this;
+    }
+};q
+
+String.prototype.toInt = function() {
+    return Number(this);
+};
+
+String.prototype.toBoolean = function() {
+   return this == true;
+};
+
+String.prototype.trimAllLine = function() {
+    return this.split("\n").map(e => {
+        return e.trim();
+    }).join("\n");
+};
+
+String.prototype.contains = function(text) {
+    return this.includes(text);
+};
+
+String.prototype.replaceAll = function(a, b) {
+    return this.split(a).join(b);
+};
+
+String.prototype.replaceLast = function(a, b) {
+    if (this.contains(a)) {
+        let lastIndex = this.lastIndexOf(a);
+        let string1 = this.substring(0, lastIndex);
+        let string2 = this.substring((lastIndex + a.length), this.length);
+        return string1 + b + string2;
+    } else return this;
+};
+
+function arrayOf(){
+    let array = [];
+    for (let i = 0; i < arguments.length; i++) {
+        array.push(arguments[i]);
+    }
+    return array;
+};
+
+const arrayOfNulls = (size) => new Array(size).join(null);
